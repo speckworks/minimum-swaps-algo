70437fefc7d403298c018aff4093643949758f6f
John K. Speck <speckworks@gmail.com>
2020-06-10 20:23:53 +0000

    elegance embodied

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const minimumSwaps = (arr)=>{
     return count;
 }
 
-let arr =  [7, 1, 3, 2, 4, 5, 6];
+let arr =  [2,1,3];
 minimumSwaps(arr)
-
+let arr1 = [7, 1, 3, 2, 4, 5, 6];
+minimumSwaps(arr1)
 
